91088e8db22b09e82a7a7eaf3c32061bc0ef553c
lee dongha <ydh20050406@gmail.com>
2025-11-12 21:25:57 +0900

    11

--- geo/utils.py
+++ geo/utils.py
@@ -1,7 +1,11 @@
 import math
-def pythagoras(a,b);
-c=math,sqrt(a**2+b**2)
-return c
-def circle(r)
-area=math.pi*r**2
-return area
+
+def pythagoras(a, b):
+    """Return the hypotenuse of a right triangle."""
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def circle(r):
+    """Return the area of a circle with radius r."""
+    area = math.pi * r**2
+    return area
